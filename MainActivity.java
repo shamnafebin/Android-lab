a3f2bd6856f05036016c67dc7c02c27c99076b7f
shamnafebin <97646487+shamnafebin@users.noreply.github.com>
2022-03-17 02:06:45 -0700

    new mulplication

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,55 @@
-package com.shamnaf.spinnertoast;
+package com.shamnaf.multiplication;
 
 import android.os.Bundle;
-
-
-
-
-
 import android.app.Activity;
 import android.view.Menu;
-
-
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-
-
-
-
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
- Spinner sp;
-	   private String[] spinnerarray;
-
+	EditText editText;
+	Button button;
+	TextView textView1,textView2;
+	int num,mul,res;
+	String str="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
-        sp=(Spinner)findViewById(R.id.spinner1);
-        this.spinnerarray =new String[]{"apple","banana","orange","pinapple"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,spinnerarray);
-        sp.setAdapter(adapter);
-        
-        
-        
+        editText=(EditText)findViewById(R.id.editText1);
+        textView1=(TextView)findViewById(R.id.textView1);
+        textView2=(TextView)findViewById(R.id.textView2);
+        button=(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+		
+			@Override
+			public void onClick(View arg0) {
+				
+				if(editText.length()>0){
+					
+					num=Integer.parseInt(editText.getText().toString());
+					textView1.setText("Multiplication Table of "+num);
+					for(int i=0;i<=10;i++){
+						mul=i;
+						res=num*mul;
+						str+= "\n"+mul+"x"+num+"="+res;
+					}
+					textView2.setText(str);
+					str="";
+				}else{
+					str="Enter Some Value";
+					textView1.setText(str);
+					str="";
+				}
+				
+			}
+		});
     }
-    
 
 
     @Override
@@ -44,10 +58,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-     
-   
-    	
-    
-   
     
 }
