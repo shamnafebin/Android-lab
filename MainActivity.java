269bb6f8f63afc26aa76b0fd54baf292da535d80
shamnafebin <97646487+shamnafebin@users.noreply.github.com>
2022-03-17 02:07:32 -0700

    add_numbers

--- MainActivity.java
+++ MainActivity.java
@@ -1,62 +1,42 @@
-package com.shamnaf.multiplication;
-
-import android.os.Bundle;
+package com.shamnaf.addtwono;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import  android.view.View.OnClickListener;
+import  android.widget.Button;
+import  android.widget.EditText;
+import  android.widget.TextView;
+
 
 public class MainActivity extends Activity {
-	EditText editText;
-	Button button;
-	TextView textView1,textView2;
-	int num,mul,res;
-	String str="";
+	EditText n1,n2;
+	Button b1;
+	TextView res;
+	int a,b,sum;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText=(EditText)findViewById(R.id.editText1);
-        textView1=(TextView)findViewById(R.id.textView1);
-        textView2=(TextView)findViewById(R.id.textView2);
-        button=(Button)findViewById(R.id.button1);
-        button.setOnClickListener(new OnClickListener() {
-		
-			@Override
-			public void onClick(View arg0) {
-				
-				if(editText.length()>0){
-					
-					num=Integer.parseInt(editText.getText().toString());
-					textView1.setText("Multiplication Table of "+num);
-					for(int i=0;i<=10;i++){
-						mul=i;
-						res=num*mul;
-						str+= "\n"+mul+"x"+num+"="+res;
-					}
-					textView2.setText(str);
-					str="";
-				}else{
-					str="Enter Some Value";
-					textView1.setText(str);
-					str="";
-				}
-				
-			}
-		});
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-}
+        n1=(EditText)(findViewById(R.id.txtNum1));
+        n2=(EditText)(findViewById(R.id.txtNum2));
+        res=(TextView)(findViewById(R.id.txtResult));
+        b1=(Button)(findViewById(R.id.btnAdd));
+        b1.setOnClickListener(new OnClickListener() {
+        	
+        	@Override
+        	public void onClick(View v ) {
+        		if(n1.length()>0 && n2.length()>0)
+        		{
+        			a=Integer.parseInt(n1.getText().toString());
+        		    b=Integer.parseInt(n2.getText().toString());
+        	        sum=a+b;
+        	        res.setText("Sum is "+sum);
+        }
+        else
+        	res.setText("Input please!");
+       }
+  });
+  
+  }
+  }
