f57675f3dd7e79e57223d1c07cfae82c860e1b3b
shamnafebin <97646487+shamnafebin@users.noreply.github.com>
2022-03-17 01:16:39 -0700

    Multiplication_table

--- MainActivity.java
+++ MainActivity.java
@@ -1,41 +1,61 @@
-package com.shamnaf.listview;
+package com.shamnaf.multiplication;
 
 import android.os.Bundle;
+
 import android.widget.TextView;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.view.View;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
 import android.view.Menu;
-import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
-	ListView li;
 	TextView t;
-	String[] ar={"apple","orange","pinapple","gua","watermelon","blueberry","mango"};
+	EditText e;
+	Button b;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        li=(ListView)findViewById(R.id.list);
-        t=(TextView)findViewById(R.id.txt);
-      
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  
-                android.R.layout.simple_spinner_dropdown_item,ar);  
-        li.setAdapter(adapter);
-        li.setOnItemClickListener(new AdapterView.OnItemClickListener() {  
-            @Override  
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {  
-                
-                String value=adapter.getItem(position);  
-                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();  
+        e=(EditText)findViewById(R.id.edt);
+        t=(TextView)findViewById(R.id.t);
+        t.setText("enter the number");
+       
+        b=(Button)findViewById(R.id.btn);
+        b.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				switch (v.getId()){
+					case R.id.btn:
+						StringBuffer bf=new StringBuffer();
+						
+						String s=e.getText().toString();
+						int n=Integer.parseInt(s);
+						for(int i=1;i<=10;i++)
+							{
+							int ans=i*n;
+							bf.append(i+"x"+n+"="+ans+"\n");
+							
+							}
+					 t.setText(bf);
+					break;
+					
+				}
+			}
+		});
     }
-        });
 
 
-   
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
     
 }
