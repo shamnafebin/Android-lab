bc5f79b0a16e57c9be899f6575d749abb891e413
shamnafebin <97646487+shamnafebin@users.noreply.github.com>
2022-03-17 01:13:55 -0700

    check_box

--- MainActivity.java
+++ MainActivity.java
@@ -1,40 +1,65 @@
-package com.shamnaf.alertbox;
+package com.shamnaf.checkbox;
 
 import android.os.Bundle;
-import android.app.AlertDialog;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
+
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Button;
 
+import android.app.Activity;
+
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	CheckBox c1,c2,c3;
 	Button b;
+	TextView t;
 	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        c1=(CheckBox)findViewById(R.id.cbox1);
+        c2=(CheckBox)findViewById(R.id.cbox2);
+        c3=(CheckBox)findViewById(R.id.cbox3);
         b=(Button)findViewById(R.id.btn);
+        t=(TextView)findViewById(R.id.txt);
+        
         b.setOnClickListener(new OnClickListener(){
         	 @Override
-        	 public void onClick(View v){
-        		 AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
-        		 builder.setCancelable(true);
-        		 builder.setMessage("this is an alert box");
-        		 builder.setPositiveButton("ok",null);
-        		 builder.setNeutralButton("cancel",null);
-        		 AlertDialog myalert=builder.create();
-        		 myalert.show();
-        		 
-        	 }
+        	public void onClick(View v){
+        		String arr="";
+        		if(c1.isChecked())
+        		{
+        			arr+="\n apple is selected";
+        		}
+        		if(c2.isChecked())
+        		{
+        			arr+="\n banana is selected";
+        		}
+        		if(c3.isChecked())
+        		{
+        			arr+="\n orange is selectd";
+        			
+        		}
+        		t.setText(arr);
+        		
+        		
+        		
+        		
+        	}
         	 
+        		
+        	
         });
     }
+}
+    
 
 
+    
    
     
-}
+
