181642fed95b44b8e0903148b1f6b7ee2ac86d31
shamnafebin <97646487+shamnafebin@users.noreply.github.com>
2022-03-17 01:15:34 -0700

    List_view

--- MainActivity.java
+++ MainActivity.java
@@ -1,59 +1,41 @@
-package com.shamnaf.dialogbox;
+package com.shamnaf.listview;
 
 import android.os.Bundle;
-
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.DialogFragment;
-import android.view.Menu;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.view.View;
-import android.widget.Button;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import android.app.AlertDialog; 
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.view.View.OnClickListener;
-
+import android.widget.ListView;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	Button b;
+	ListView li;
+	TextView t;
+	String[] ar={"apple","orange","pinapple","gua","watermelon","blueberry","mango"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b=(Button)findViewById(R.id.btndialog);
-        b.setOnClickListener(new OnClickListener(){
-     
-			 public void onClick(View v) {
-				// TODO Auto-generated method stub
-			   	 ShowDateTimeDialog dt=new ShowDateTimeDialog();
-        		 dt.show(getFragmentManager(),null);
-			}
-        	
+        li=(ListView)findViewById(R.id.list);
+        t=(TextView)findViewById(R.id.txt);
+      
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  
+                android.R.layout.simple_spinner_dropdown_item,ar);  
+        li.setAdapter(adapter);
+        li.setOnItemClickListener(new AdapterView.OnItemClickListener() {  
+            @Override  
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {  
+                
+                String value=adapter.getItem(position);  
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();  
+    }
         });
-    }
-   public class ShowDateTimeDialog extends DialogFragment{
-	   @Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState){
-    		 Calendar c=Calendar.getInstance();
-    		 SimpleDateFormat sdf=new SimpleDateFormat("dd-MMM-yyy HH:mm:ss");
-    		 String strDate=sdf.format(c.getTime());
-    		 AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-    		 builder.setTitle("Date and Time");
-    		 builder.setMessage(strDate);
-    		 builder.setNeutralButton("ok",null);
-    		 return builder.create();
-    		 
-    		 
-    		 
-    		 
-    		 
-    	 }
-    	
-    }
 
 
+   
+    }
     
 }
